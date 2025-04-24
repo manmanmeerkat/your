4ceea57db1639c656ebbbe0887ee026ec9a32a6d
naisaika <naisaikapoemelody@gmail.com>
2025-04-24 20:25:28 +0900

    setting-GSC

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+// next-sitemap.config.js
+import { IConfig } from 'next-sitemap'
+
+/** @type {IConfig} */
+const config = {
+  siteUrl: 'https://your-usrb.vercel.app',
+  generateRobotsTxt: true,
+};
+
+export default config;
