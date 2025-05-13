84763d4b714697aa1ee716ff083557f49df89869
naisaika <naisaikapoemelody@gmail.com>
2025-05-13 20:53:05 +0900

    change the link design in the article pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,32 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       screens: {
-        xs: '377px',
+        xs: "377px",
       },
       spacing: {
         18: "4.5rem",
       },
+      typography: (theme) => ({
+        invert: {
+          css: {
+            a: {
+              color: theme("colors.rose.400"),
+              "text-decoration": "underline",
+              "text-underline-offset": "4px",
+              transition: "color 0.3s ease",
+              "&:hover": {
+                color: theme("colors.rose.300"),
+              },
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [typography],
-}
+};
